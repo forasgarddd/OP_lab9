8b4cf82cb88c60e185ca519908549490d66f5df9
Ivan Devitskyi <forbattlenetonly14@gmail.com>
2020-11-28 00:58:25 +0200

    Added String check method for hex in Task 4

--- OP-lab9/src/lab9/Task4.java
+++ OP-lab9/src/lab9/Task4.java
@@ -4,15 +4,34 @@ public class Task4 {
 
     public static void main(String[] args) {
         printResult("CAFE");
-        //printRes("ABCD");
+        printResult("LOST");
+        printResult("");
+    }
+
+    static boolean isHex(String s) {
+        boolean ret;
+        try {
+            Integer.parseInt(s, 16);
+            ret = true;
+        } catch (NumberFormatException ex) {
+            ret = false;
+        }
+        return ret;
     }
 
     static int hexStringToInt(String s) {
+        if (isHex(s) == false) {
+            throw new NumberFormatException("Given string is not hex");
+        }
         return Integer.parseInt(s, 16);
     }
 
     static void printResult(String s) {
-        System.out.println("Given string in hex: " + s);
-        System.out.println("Converted to int: " + hexStringToInt(s));
+        try {
+            System.out.println("Given string: " + s);
+            System.out.println("Converted to int: " + hexStringToInt(s));
+        } catch (NumberFormatException ex) {
+            System.out.println("EXCEPTION! " + ex.getMessage());
+        }
     }
 }
