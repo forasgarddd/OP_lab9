de9318fead31cc5061f39273336cbaa218cd5864
Ivan Devitskyi <forbattlenetonly14@gmail.com>
2020-11-28 01:04:28 +0200

    Added check for empty string in Task 10

--- OP-lab9/src/lab9/Task10.java
+++ OP-lab9/src/lab9/Task10.java
@@ -8,9 +8,13 @@ public class Task10 {
 
     public static void main(String[] args) {
         printResult("The user with the nickname koala757677 this month wrote 3 times more comments than the user with the nickname croco181dile920 4 months ago");
+        printResult("");
     }
 
     static String wordsInStringToUpperCase(String s) {
+        if (s.length() == 0) {
+            throw new IllegalArgumentException("Given string is empty");
+        }
         String upper_s = s.toUpperCase();
         List<String> s_arrlist = new ArrayList<>(Arrays.asList(upper_s.split(" ")));
         for (String word : s_arrlist) {
@@ -24,7 +28,11 @@ public class Task10 {
     }
 
     static void printResult(String s) {
-        System.out.println("Given string: " + s);
-        System.out.println("String with uppercase words: " + wordsInStringToUpperCase(s));
+        try {
+            System.out.println("Given string: " + s);
+            System.out.println("String with uppercase words: " + wordsInStringToUpperCase(s));
+        } catch (IllegalArgumentException ex) {
+            System.out.println("EXCEPTION! " + ex.getMessage());
+        }
     }
 }
