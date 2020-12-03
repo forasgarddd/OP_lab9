f4ae6eb06e4b6cd9ef5063af4a3a6c0526628bbe
Ivan Devitskyi <forbattlenetonly14@gmail.com>
2020-12-03 10:40:20 +0200

    minor commit

--- OP-lab9/src/lab9/Task10.java
+++ OP-lab9/src/lab9/Task10.java
@@ -8,7 +8,7 @@ public class Task10 {
 
     public static void main(String[] args) {
         printResult("The user with the nickname koala757677 this month wrote 3 times more comments than the user with the nickname croco181dile920 4 months ago");
-        printResult("");
+        printResult("He11o Wor1d!");
     }
 
     static String wordsInStringToUpperCase(String s) {
@@ -18,7 +18,7 @@ public class Task10 {
         String upper_s = s.toUpperCase();
         List<String> s_arrlist = new ArrayList<>(Arrays.asList(upper_s.split(" ")));
         for (String word : s_arrlist) {
-            if (word.matches("\\S*\\d+\\S")) {
+            if (word.matches("\\S*\\d+\\S*")) {
                 String nickname = word.toLowerCase();
                 int index_of_nickname = s_arrlist.indexOf(word);
                 s_arrlist.set(index_of_nickname, nickname);
